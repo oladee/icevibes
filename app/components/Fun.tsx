bc6b9cf6a202c8ed46d640db5650e480386773db
oladee <deemajor230600@gmail.com>
2024-12-15 10:34:02 +0100

    doney

---
+++ app/components/Fun.tsx
@@ -0,0 +1,19 @@
+const Fun = () => {
+  return (
+    <div className="lg:py-[120px] lg:px-[8.33vw]">
+      <div className="lg:flex lg:justify-between lg:items-center">
+        <h2 className="font-beachday lg:w-[30.2vw] lg:text-[3.88vw]">
+        All the <span className="text-[#FDAC18]">fun</span> you can imagine
+        </h2>
+        <p className="font-switzer lg:text-[1.38vw] lg:w-[42.84vw]">
+        We bring together the thrill of ice skating, the excitement of arcade gaming, and the energy of live entertainment into one unforgettable event. Over the course of a month, our venue will transform into a buzzing hub of activity, perfect for anyone looking to create magical memories this holiday season
+        </p>
+      </div>
+      <div className="lg:my-16 lg:py-9 bg-[#BBE4FF] border-t border-r-8 border-b-8 border-l-2 border-[#000] rounded-2xl">
+
+      </div>
+    </div>
+  )
+}
+
+export default Fun
