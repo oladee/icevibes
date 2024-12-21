9100129a6f15ebedb21c7e339333a830222d3f88
oladee <deemajor230600@gmail.com>
2024-12-21 08:18:52 +0100

    doney

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -75,7 +75,7 @@ const Footer = ()=>{
     return(<div className='bg-[#000] py-16 px-5 lg:py-[120px] lg:px-[8.33vw] relative'>
         <div className="lg:px-20 w-full relative bg-red-600">
             <video preload="none" ref={videoRef} autoPlay muted disablePictureInPicture loop playsInline width={320} height={320} className="lg:h-[501px] lg:w-[69.44vw] object-cover rounded-xl z-50">
-                <source src="/assets/video.mp4" type="video/mp4" />
+                <source src="/assets/clip1.mp4" type="video/mp4" />
             </video>
 
             <div className="absolute bottom-0 right-0 lg:right-28 cursor-pointer">
